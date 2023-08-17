590dc246ef4f5696d5e0e72c8e49a0ee1cf263fb
Spaceginner <ivan.demian2009@gmail.com>
2023-08-17 14:26:17 +0500

    allow getting user data via admin endpoints

--- arcos_backend/routers/v1/_schemas.py
+++ arcos_backend/routers/v1/_schemas.py
@@ -1,6 +1,16 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class UserEdit(BaseModel):
     password: str | None = None
     state: bool | None = None
+
+
+class UserData(BaseModel):
+    id: int
+    username: str
+    properties: dict
+    creation_time: datetime
+    is_deleted: bool
--- arcos_backend/routers/v1/admin.py
+++ arcos_backend/routers/v1/admin.py
@@ -1,11 +1,11 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from . import user
 from ._common import get_db, auth_admin
-from ._schemas import UserEdit
+from ._schemas import UserEdit, UserData
 from .. import EndpointTags
 from ...davult.crud import user as user_db
 
@@ -27,3 +27,23 @@ def admin_change_user(_: Annotated[None, Depends(auth_admin)], db: Annotated[Ses
 
     if edit.state is not None:
         user_db.set_user_state(db, user, edit.state)
+
+
+@router.get('/user', summary="Get user's data")
+# FIXME read da fastapi docs - maybe there is a way to describe such situation
+def admin_get_user(_: Annotated[None, Depends(auth_admin)], db: Annotated[Session, Depends(get_db)], name: str | None = None, id: int | None = None):
+    if not (bool(name) ^ bool(id)):
+        raise HTTPException(status_code=422, detail="provide only either name or id")
+
+    if name:
+        user = user_db.find_user(db, name)
+    else:
+        user = user_db.get_user(db, id)
+
+    return {
+        'id': user.id,
+        'username': user.username,
+        'properties': user.properties,
+        'creation_time': user.creation_time,
+        'is_deleted': user.is_deleted
+    }
