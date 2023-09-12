6bc27caac33d1af1f632e8ec98f96afe5516ce1d
Spaceginner <ivan.demian2009@gmail.com>
2023-09-12 14:03:29 +0500

    add `/connect` endpoint in `authcode` exclusion

--- arcos_backend/authentication.py
+++ arcos_backend/authentication.py
@@ -2,6 +2,8 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import Response
 
+EXCLUDED_ENDPOINTS = ['/connect']
+
 
 class AuthCodeMiddleware(BaseHTTPMiddleware):
     def __init__(self, app, authcode: str):
@@ -10,15 +12,16 @@ class AuthCodeMiddleware(BaseHTTPMiddleware):
         self._authcode = authcode
 
     async def dispatch(self, request: Request, call_next):
-        if self._authcode is None:
-            return await call_next(request)
+        if (
+                self._authcode is not None and
+                request.scope['path'] not in EXCLUDED_ENDPOINTS
+        ):
+            params = request.query_params
 
-        params = request.query_params
+            if 'ac' not in params:
+                return Response(status_code=401)
 
-        if 'ac' not in params:
-            return Response(status_code=401)
-
-        if params['ac'] != self._authcode:
-            return Response(status_code=403)
+            if params['ac'] != self._authcode:
+                return Response(status_code=403)
 
         return await call_next(request)
