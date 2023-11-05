dcdcbc4abfcb50ac3396be46bd067beb56712245
Spaceginner <ivan.demian2009@gmail.com>
2023-11-05 19:13:54 +0200

    remove rate limiting of `/users/get`

--- arcos_backend/routers/v1/users.py
+++ arcos_backend/routers/v1/users.py
@@ -1,23 +1,19 @@
 import json
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
 
 from ._common import get_db
 from .. import EndpointTags
 from ...davult.models import is_enabled
 from ...davult.crud import user as user_db
 
-limiter = Limiter(key_func=get_remote_address)
 router = APIRouter(tags=[EndpointTags.users])
 
 
 @router.get('/get', summary="Get the list of users")
-def users_get(request: Request, db: Annotated[Session, Depends(get_db)]):
+def users_get(db: Annotated[Session, Depends(get_db)]):
     users = user_db.get_users(db)
 
     return {
