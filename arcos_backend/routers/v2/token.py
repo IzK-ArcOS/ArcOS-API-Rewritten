f4423faea8f741d04e055941fe1a41af54443383
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-11-29 20:10:34 +0100

    Rename `session` to `token`

--- arcos_backend/routers/v2/token.py
+++ arcos_backend/routers/v2/token.py
@@ -1,16 +1,14 @@
 from typing import Annotated
-
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-
 from .._common import get_db
 from ...davult.crud import token as token_db, user as user_db
 from ...davult.models import Token
 from ...davult.schemas import TokenCreate
 from ._auth import OAuth2LoginInfo, get_token
 
-router = APIRouter()
+token_api = APIRouter()
 
 
 # TODO move it into
@@ -18,22 +16,40 @@ DEFAULT_TOKEN_LIFETIME = 60 * 60 * 24 * 7  # 1 week
 
 
 # TODO scopes support
-@router.post('/')
-async def create_session(db: Annotated[Session, Depends(get_db)], login_info: Annotated[OAuth2PasswordRequestForm, Depends()]) -> OAuth2LoginInfo:
+@router.post("/")
+async def create_session(
+    db: Annotated[Session, Depends(get_db)],
+    login_info: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> OAuth2LoginInfo:
     try:
         user = user_db.find_user(db, login_info.username)
+
     except LookupError:
         # we do not do 401, as by using listing of all users it is possible to know if username or password is wrong
+
         raise HTTPException(status_code=404, detail="user not found") from None
+
     else:
         try:
-            token = token_db.generate_token(db, TokenCreate(owner_id=user.id, lifetime=DEFAULT_TOKEN_LIFETIME, password=login_info.password))
+            token = token_db.generate_token(
+                db,
+                TokenCreate(
+                    owner_id=user.id,
+                    lifetime=DEFAULT_TOKEN_LIFETIME,
+                    password=login_info.password,
+                ),
+            )
+
         except ValueError:
-            raise HTTPException(status_code=401, detail="password is invalid") from None
+            raise HTTPException(
+                status_code=401, detail="password is invalid") from None
+
         else:
             return OAuth2LoginInfo(access_token=token.value)
 
 
-@router.delete('/')
-async def invalidate_session(db: Annotated[Session, Depends(get_db)], token: Annotated[Token, Depends(get_token)]) -> None:
+@router.delete("/")
+async def invalidate_token(
+    db: Annotated[Session, Depends(get_db)], token: Annotated[Token, Depends(get_token)]
+) -> None:
     token_db.expire_token(db, token)
