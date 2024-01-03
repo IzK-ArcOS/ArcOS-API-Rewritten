0888858709389a84b37363adb270c129589ed33a
Levi G <bobrobot@zoho.com>
2024-01-03 11:14:31 -0800

    bugfix

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     cfg = arcos_backend.get_cfg()
     uvicorn.run(
         arcos_backend.app,
-        host="0.0.0.0" if cfg['info']['listen'] or os.environ['ARCOS_LISTEN_CONFIG_OVERRIDE'] == "true" else "localhost",
+        host="0.0.0.0" if cfg['info']['listen'] or os.getenv('ARCOS_LISTEN_CONFIG_OVERRIDE') == "true" else "localhost",
         port=cfg['info']['port']
     )
